1dd9a738c2d4211145f45b8ba5f07dbda452367e
Kylin <kylin87@qq.com>
2019-12-20 11:37:52 +0000

    添加admin应用

---
+++ app/admin/controller/Login.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\BaseController;
+use think\facade\View;
+
+class Login extends BaseController
+{
+    public function index() {
+    	return View::fetch();
+    }
+}

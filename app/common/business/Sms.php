e5c2d4d8355cba715a7b749f6be7ccd315e6f66f
Kylin <kylin87@qq.com>
2020-01-16 05:57:06 +0000

    7-11 代码高度优化-利用反射机制处理工厂模式做到真正的高大上思想

--- app/common/business/Sms.php
+++ app/common/business/Sms.php
@@ -5,6 +5,7 @@ namespace app\common\business;
 
 use app\common\lib\sms\AliSms;
 use app\common\lib\Num;
+use app\common\lib\ClassArr;
 
 /**
  * 发送短信业务逻辑
@@ -18,9 +19,13 @@ class Sms
         //$sms = AliSms::sendCode($phoneNumber, $code);
 
         //工厂模式
-        $type = ucfirst($type);
-        $class = "app\common\lib\sms\\".$type."Sms";
-        $sms = $class::sendCode($phoneNumber, $code);
+        // $type = ucfirst($type);
+        // $class = "app\common\lib\sms\\".$type."Sms";
+        // $sms = $class::sendCode($phoneNumber, $code);
+
+        $classStats = ClassArr::smsClassStat();
+        $classObj = ClassArr::initClass($type, $classStats);
+        $sms = $classObj::sendCode($phoneNumber, $code);
         if ($sms) {
             //需要把短信验证码记录到Redis，并且给出一个失效时间
             cache(config('redis.code_pre').$phoneNumber, $code, config('redis.code_expire'));
